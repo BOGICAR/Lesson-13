51463ee6bc692e4a44a7246e8a55cec23a5594f9
BOGICAR <85548214+BOGICAR@users.noreply.github.com>
2021-08-10 18:12:18 +0300

    Lesson №13
    Task 1

--- coffee_shop.py
+++ coffee_shop.py
@@ -1,77 +1,79 @@
 import csv
 
 
+class Product:
+
+    def __init__(self, prod_name, prod_type, price):
+        self.prod_name = prod_name
+        self.prod_type = prod_type
+        self.price = price
+
+    def __str__(self):
+        return f'{self.prod_type}: {self.prod_name} - price:{self.price}.'
+
+    def __repr__(self):
+        return self.__str__()
+
+
 class Store:
 
     def __init__(self):
         self.list_product = []
+        self.balance = 0
         self.import_times = 5
 
+    def add_product(self, raw, times):
+        self.list_product.extend([Product(raw['Наименование'],
+                                          raw['Тип'],
+                                          float(raw['Цена']))
+                                 for i in range(times)])
+        return self.list_product
+
     def read_csv(self):
-        with open('inventory.csv', 'r', encoding='utf-8') as f:
-            file_reader = csv.reader(f, delimiter=',')
-            next(file_reader)
-            for i in file_reader:
-                self.list_product.append(i)
-            self.list_product *= self.import_times
+        with open('inventory.csv', 'r', encoding='utf-8') as file:
+            file_reader = csv.DictReader(file, delimiter=',')
+            for row in file_reader:
+                self.add_product(row, self.import_times)
 
     def tipe_list(self, tipe=None):
         self.tipe_list = []
-        for line in self.list_product:
-            if line[1] == tipe:
-                self.tipe_list.append(line[0])
-            if tipe == None:
-                self.tipe_list.append(line[0])
+        for i in self.list_product:
+            if tipe == i.prod_type:
+                self.tipe_list.append(i)
+            if tipe is None:
+                self.tipe_list.append(i)
         return self.tipe_list
 
     def cost_products(self):
         self.cost_products = 0
         self.result = ''
-        for line in self.list_product:
-            self.cost_products += int(line[2])
+        for prod in self.list_product:
+            self.cost_products += prod.price
             self.result = f'Общая стоимость продуктов в наличии: {self.cost_products} грн.'
         return self.result
 
     def sale_product(self, name):
-        self.sale_list = []
-        for line in self.list_product:
-            if line[0] == name:
-                self.sale_list.append(line)
-                self.list_product.remove(line)
-                return self.sale_list
+        self.prod_index = None
+        for i, prod in enumerate(self.list_product):
+            if prod.prod_name == name:
+                self.prod_index = i
+                break
+        if self.prod_index is not None:
+            self.balance += self.list_product[self.prod_index].price
+            self.list_product.pop(self.prod_index)
+            print(f'{name} - продан')
+        else:
+            print('Выберите доступный продукт')
 
     def balance(self):
-        self.balance = 0
-        for list in self.sale_list:
-            self.balance += int(list[2])
         return self.balance
 
 
-class Product(Store):
-
-    def properties(self, name):
-        self.result = ''
-        for line in self.list_product:
-            if line[0] == name:
-                self.result = line
-                # self.result = f'Наименование: {line[0]}, ' \
-                #               f'тип: {line[1]}, ' \
-                #               f'цена: {line[2]}'
-                return self.result
-
-    def print_object(self, name):
-        for line in self.list_product:
-            if line[0] == name:
-                self.result = f'Товар: {name}, цена: {line[2]}'
-                return self.result
-
-
-test = Product()
-test.read_csv()
 # Проверка работы
-print(test.properties('Доппио'))
-print(test.print_object('Earl Grey'))
-print(test.tipe_list('coffee'))
-print(test.cost_products())
-print(test.sale_product('Имбирный чай'))
-print(test.balance())
+x = Store()
+x.read_csv()
+print(x.list_product)
+print(x.cost_products())
+x.sale_product('Эспрессо')
+print(x.tipe_list('tea'))
+print(x.balance)
